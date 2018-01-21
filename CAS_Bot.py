2cb388e416c7cc2d61d6c2b94952d7e7ae3058d1
orangeismyfavorite <xsoltis@gmail.com>
2018-01-21 18:17:39 -0500

    Update CAS_Bot.py

--- CAS_Bot.py
+++ CAS_Bot.py
@@ -6,10 +6,10 @@ import requests
 def botlogin():
 	print ("Logging in...")
 	r = praw.Reddit(username = "username",
-	      password = "password",
-	      client_id = "client_id",
-	      client_secret = "client_secret",
-	      user_agent = "user_agent")
+				password = "password",
+				client_id = "client id",
+				client_secret = "client secret",
+				user_agent = "user agent")
 	print("Logged in.")
 	return r
 
@@ -20,35 +20,144 @@ def run_bot(r, comments_replied_to):
 	movie = []
 	print("Obtaining up to " + str(n) + " comments...")
 	for comment in r.subreddit('CAS_Bot+test').comments(limit=n):
-		if "!media " in comment.body and comment not in comments_replied_to and comment.author != r.user.me():
-			print("String containing \"!media\" found!")
+		###gets the rating
+		if "!rating" in comment.body and comment not in comments_replied_to and comment.author != r.user.me():
+			print("String containing \"!rating\" found!")
 
 			###gets words after '!movie' and turns it into a usable format.
-			keyphrase = comment.body[comment.body.find("!movie")+1:][6::]
+			keyphrase = comment.body[comment.body.find("!movie")+1:][7::]
 			keyphrase = keyphrase.split()
 			keyphrase = '+'.join(keyphrase)
 
 			###checks if the movie requested exists. if it does, it gives the expected response.
 			try:
-				info = requests.get("http://www.omdbapi.com/?apikey=apikey=" + keyphrase).json()['Plot']
-				comment_reply = "Here is a plot summary:\n\n"			
-				comment_reply += ">" + info
+				info = requests.get("http://www.omdbapi.com/?apikey=apikey&t=" + keyphrase).json()['Ratings'][1]['Value']
+				comment_reply = "Here is the rating:\n\n"			
+				comment_reply += ">" + "Rotten Tomatoes: " + info
 				comment_reply += "\n\n*****"
 				comment_reply += "\n\n" + " "
-				comment_reply += "\n\n ^^Info ^^from ^^[omdbapi.com](https://omdbapi.com)." + " ^^I ^^am ^^a ^^bot ^^created ^^by ^^/u/81isnumber1. ^^Instructions ^^can ^^be ^^found ^^[here](https://github.com/orangeismyfavorite/CAS_Bot/blob/master/README.md"
+				comment_reply += "\n\n ^^Info ^^from ^^[omdbapi.com](https://omdbapi.com)." + " ^^I ^^am ^^a ^^bot ^^created ^^by ^^/u/81isnumber1. ^^Instructions ^^can ^^be ^^found ^^[here](https://github.com/orangeismyfavorite/CAS_Bot/blob/master/README.md)."
 
 				comment.reply(comment_reply)
 				comments_replied_to.append(comment.id)
 
 			except Exception:
 				comments_replied_to.append(comment.id)
-				print("That movie didn't exist! Just going to ignore that...")
+				print("That didn't exist! Just going to ignore that...")
 				pass
+		###gets the plot
+		elif "!plot" in comment.body and comment not in comments_replied_to and comment.author != r.user.me():
+				print("String containing \"!plot\" found!")
 
-			###puts the comment id into a text file so a check makes sure to not respond to
-			###the same comment twice.
-			with open("comments_replied_to.txt", "a") as f:
-					f.write(comment.id + "\n")
+				###gets words after '!movie' and turns it into a usable format.
+				keyphrase = comment.body[comment.body.find("!movie")+1:][5::]
+				keyphrase = keyphrase.split()
+				keyphrase = '+'.join(keyphrase)
+
+				###checks if the movie requested exists. if it does, it gives the expected response.
+				try:
+					info = requests.get("http://www.omdbapi.com/?apikey=apikey&t=" + keyphrase).json()['Plot']
+					comment_reply = "Here is a plot summary:\n\n"			
+					comment_reply += ">" + info
+					comment_reply += "\n\n*****"
+					comment_reply += "\n\n" + " "
+					comment_reply += "\n\n ^^Info ^^from ^^[omdbapi.com](https://omdbapi.com)." + " ^^I ^^am ^^a ^^bot ^^created ^^by ^^/u/81isnumber1. ^^Instructions ^^can ^^be ^^found ^^[here](https://github.com/orangeismyfavorite/CAS_Bot/blob/master/README.md)."
+
+					comment.reply(comment_reply)
+					comments_replied_to.append(comment.id)
+
+				except Exception:
+					comments_replied_to.append(comment.id)
+					print("That didn't exist! Just going to ignore that...")
+					pass
+		###gets the actors
+		elif "!cast" in comment.body and comment not in comments_replied_to and comment.author != r.user.me():
+				print("String containing \"!actors\" found!")
+
+				###gets words after '!movie' and turns it into a usable format.
+				keyphrase = comment.body[comment.body.find("!movie")+1:][5::]
+				keyphrase = keyphrase.split()
+				keyphrase = '+'.join(keyphrase)
+
+				###checks if the movie requested exists. if it does, it gives the expected response.
+				try:
+					info = requests.get("http://www.omdbapi.com/?apikey=apikey&t=" + keyphrase).json()['Actors']
+					comment_reply = "Here are the actors:\n\n"			
+					comment_reply += ">" + info
+					comment_reply += "\n\n*****"
+					comment_reply += "\n\n" + " "
+					comment_reply += "\n\n ^^Info ^^from ^^[omdbapi.com](https://omdbapi.com)." + " ^^I ^^am ^^a ^^bot ^^created ^^by ^^/u/81isnumber1. ^^Instructions ^^can ^^be ^^found ^^[here](https://github.com/orangeismyfavorite/CAS_Bot/blob/master/README.md)."
+
+					comment.reply(comment_reply)
+					comments_replied_to.append(comment.id)
+
+				except Exception:
+					comments_replied_to.append(comment.id)
+					print("That didn't exist! Just going to ignore that...")
+					pass
+
+
+		elif "!boxoffice" in comment.body and comment not in comments_replied_to and comment.author != r.user.me():
+				print("String containing \"!boxoffice\" found!")
+
+				###gets words after '!movie' and turns it into a usable format.
+				keyphrase = comment.body[comment.body.find("!movie")+1:][10::]
+				keyphrase = keyphrase.split()
+				keyphrase = '+'.join(keyphrase)
+
+				###checks if the movie requested exists. if it does, it gives the expected response.
+				try:
+					info = requests.get("http://www.omdbapi.com/?apikey=apikey&t=" + keyphrase).json()['BoxOffice']
+					comment_reply = "Here is the Box Office:\n\n"			
+					comment_reply += ">" + info
+					comment_reply += "\n\n*****"
+					comment_reply += "\n\n" + " "
+					comment_reply += "\n\n ^^Info ^^from ^^[omdbapi.com](https://omdbapi.com)." + " ^^I ^^am ^^a ^^bot ^^created ^^by ^^/u/81isnumber1. ^^Instructions ^^can ^^be ^^found ^^[here](https://github.com/orangeismyfavorite/CAS_Bot/blob/master/README.md)."
+
+					comment.reply(comment_reply)
+					comments_replied_to.append(comment.id)
+
+				except Exception:
+					comments_replied_to.append(comment.id)
+					print("That didn't exist! Just going to ignore that...")
+					pass
+
+
+
+
+		elif "!director" in comment.body and comment not in comments_replied_to and comment.author != r.user.me():
+				print("String containing \"!director\" found!")
+
+				###gets words after '!movie' and turns it into a usable format.
+				keyphrase = comment.body[comment.body.find("!movie")+1:][10::]
+				keyphrase = keyphrase.split()
+				keyphrase = '+'.join(keyphrase)
+
+				###checks if the movie requested exists. if it does, it gives the expected response.
+				try:
+					info = requests.get("http://www.omdbapi.com/?apikey=apikey&t=" + keyphrase).json()['Director']
+					comment_reply = "Here is the Director:\n\n"			
+					comment_reply += ">" + info
+					comment_reply += "\n\n*****"
+					comment_reply += "\n\n" + " "
+					comment_reply += "\n\n ^^Info ^^from ^^[omdbapi.com](https://omdbapi.com)." + " ^^I ^^am ^^a ^^bot ^^created ^^by ^^/u/81isnumber1. ^^Instructions ^^can ^^be ^^found ^^[here](https://github.com/orangeismyfavorite/CAS_Bot/blob/master/README.md)."
+
+					comment.reply(comment_reply)
+					comments_replied_to.append(comment.id)
+
+				except Exception:
+					comments_replied_to.append(comment.id)
+					print("That didn't exist! Just going to ignore that...")
+					pass
+
+
+
+
+
+				###puts the comment id into a text file so a check makes sure to not respond to
+				###the same comment twice.
+				with open("comments_replied_to.txt", "a") as f:
+						f.write(comment.id + "\n")
 
 
 
